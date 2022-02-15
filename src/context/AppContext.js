7056577175edeb1b4363ef48b1b9cab7f5a2f80a
Kayle Rieger Patton <kayle.patton22@gmail.com>
2022-02-15 22:11:36 +0000

    app context

---
+++ src/context/AppContext.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+// 1. Create context (boundary for accessing state)
+export const AppContext = React.createContext();
+
+export const AppProvider = ({ children }) => {
+  const [todos, setTodos] = useState(
+    JSON.parse(localStorage.getItem("todoItems"))
+  );
+  // contains all elements being set in global state
+  const providerValue = { todos, setTodos };
+
+  //   pass in global state items and inject child HTML elements
+  return (
+    <AppContext.Provider value={providerValue}>{children}</AppContext.Provider>
+  );
+};
